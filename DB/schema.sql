57920bbb8e9b733855e315e7bcf72eb36de55974
denist2322 <parksomang125@gmail.com>
2022-07-25 17:33:56 +0900

    추천 기능 추가(DB)

--- DB/schema.sql
+++ DB/schema.sql
@@ -9,846 +9,68 @@ CREATE TABLE Question (
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     `subject` VARCHAR(200) NOT NULL,
     content TEXT NOT NULL,
-    create_date DATETIME NOT NULL
+    create_date DATETIME NOT NULL,
+    modify_date DATETIME NOT NULL,
+    Answer_id INT(11) UNSIGNED,
+    question_voter_id INT(11) UNSIGNED,
+    author_id INT(11) UNSIGNED NOT NULL
 );
 
 INSERT INTO Question SET
 create_date = NOW(),
+modify_date = NOW(),
 `subject` = '질문 1',
-content = '질문내용 1';
+content = '질문내용 1',
+Answer_id = 1,
+author_id = 1;
 
 INSERT INTO Question SET
 create_date = NOW(),
+modify_date = NOW(),
 `subject` = '질문 2',
-content = '질문내용 2';
+content = '질문내용 2',
+Answer_id = 2,
+author_id = 2;
 
 INSERT INTO Question SET
 create_date = NOW(),
+modify_date = NOW(),
 `subject` = '질문 3',
-content = '질문내용 3';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
-
-INSERT INTO Question SET
-create_date = NOW(),
-`subject` = '테스트',
-content = '테스트';
+content = '질문내용 3',
+Answer_id = 3,
+author_id = 3;
 
 
 CREATE TABLE Answer (
     id INT(11) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
     content TEXT NOT NULL,
     create_date DATETIME NOT NULL,
-    question_id INT(11) UNSIGNED NOT NULL
+    modify_date DATETIME NOT NULL,
+    question_id INT(11) UNSIGNED NOT NULL,
+    answer_voter_id INT(11) UNSIGNED,
+    author_id INT(11) UNSIGNED NOT NULL
 );
 
 INSERT INTO Answer SET
 create_date = NOW(),
+modify_date = NOW(),
 content = '답변내용 1',
-question_id = 1;
+question_id = 1,
+author_id = 1;
 
 INSERT INTO Answer SET
 create_date = NOW(),
+modify_date = NOW(),
 content = '답변내용 2',
-question_id = 2;
+question_id = 2,
+author_id = 2;
 
 INSERT INTO Answer SET
 create_date = NOW(),
+modify_date = NOW(),
 content = '답변내용 3',
-question_id = 3;
+question_id = 3,
+author_id = 3;
 
 CREATE TABLE site_user(
     id INT(20) UNSIGNED NOT NULL PRIMARY KEY AUTO_INCREMENT,
@@ -860,8 +82,30 @@ CREATE TABLE site_user(
 INSERT INTO site_user SET
 username = "유저1",
 `password` = "1234",
-email = "test@test.com";
+email = "test1@test.com";
+
+INSERT INTO site_user SET
+username = "유저2",
+`password` = "1234",
+email = "test2@test.com";
+
+INSERT INTO site_user SET
+username = "유저3",
+`password` = "1234",
+email = "test3@test.com";
+
+CREATE TABLE answer_voter(
+    answer_id INT(11) UNSIGNED NOT NULL,
+    voter_id INT(11) UNSIGNED NOT NULL PRIMARY KEY
+
+);
+
+CREATE TABLE question_voter(
+    question_id INT(11) UNSIGNED NOT NULL,
+    voter_id INT(11) UNSIGNED NOT NULL PRIMARY KEY
+);
 
 SELECT * FROM Question;
 SELECT * FROM Answer;
 SELECT * FROM site_user;
+SELECT * FROM question_voter;
